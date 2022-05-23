3b8d99b92fe3ba6bd0166fd35ec05a0e4822ca05
rscres <renatocrescenti@gmail.com>
2022-05-23 19:26:21 -0300

    sizeChange function added and working

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-function squareCreator() {
-    const container = document.querySelector('#container');
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
+const container = document.querySelector('#container');
+
+function squareCreator(size = 16) {
+    container.style.gridArea= `1 / span ${size}`;
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
             let square = document.createElement('div');
             square.classList.add('box');
             square.style.gridColumn = `${1 + j}`;
@@ -13,8 +15,26 @@ function squareCreator() {
 
 squareCreator();
 
+
 const colorChange = window.addEventListener('mouseover', function(e){
-    console.log(e.target);
-    const box = e.target
-    box.style.backgroundColor = 'black';
-})
+    if (e.target.className == 'box') {
+        const box = e.target
+        box.style.backgroundColor = 'black';
+    }
+});
+
+const sizeButton = document.querySelector('#size');
+sizeButton.addEventListener('click', function(e) {
+    let sizePrompt = prompt('Enter grid size(must be smaller than 100): ')
+    if (sizePrompt > 100 || sizePrompt < 0) {
+        sizePrompt = prompt('Please try again: ');
+    } else {
+        let size = sizePrompt;
+        console.log(size);
+        let squares = document.querySelectorAll('#box');
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+        squareCreator(size);
+    }
+});
